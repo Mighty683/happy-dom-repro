59c138cbf6f17b3994bc9cae5a3ffe01be6e40fb
Mighty683 <tomasz93szarek@gmail.com>
2024-03-15 16:50:49 +0100

    fix: typo

--- src/CustomElement.test.ts
+++ src/CustomElement.test.ts
@@ -6,7 +6,7 @@ describe("CustomElement", () => {
   it("should update the button when the attribute is changed", async () => {
     const element = document.createElement("custom-element");
     document.body.appendChild(element);
-    element.setAttribute("value", "2");
+    element.setAttribute("count", "2");
     expect(element.shadowRoot?.querySelector("button")?.textContent).toBe("2");
   });
 });
